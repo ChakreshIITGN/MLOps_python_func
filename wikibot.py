fef926b9c228d530c00585d32fbb2b23e0276dd0
Chakresh Kr. Singh <34956550+ChakreshIITGN@users.noreply.github.com>
2023-11-14 11:24:17 +0000

    Adding click functionality

--- wikibot.py
+++ wikibot.py
@@ -1,10 +1,25 @@
 import wikipedia as wp
+import click
 
-def scrape(name="Microsoft",length=2):
+## this part is good for a test function but now we implement using click library which is a neater way of passing args. 
+
+# def scrape(name="Microsoft",length=2):
+
+#     result = wp.summary(name,sentences=length)
+#     return name, result
+
+# usr_input,wiki_result = scrape()
+
+# print(f"Scrape result from Wikipedia for {usr_input} is \n --- {wiki_result} --- ")
+
+@click.command()
+@click.option('--name', prompt='Wikipedia page name : ',
+              help='Scrapes the wikipedia page for the name you enter')
+def scrape(name,length=2):
 
     result = wp.summary(name,sentences=length)
-    return name, result
+    click.echo(click.style(result, bg='white', fg='cyan'))
 
-usr_input,wiki_result = scrape()
 
-print(f"Scrape result from Wikipedia for {usr_input} is \n --- {wiki_result} --- ")
+if __name__ == '__main__':
+    scrape()
